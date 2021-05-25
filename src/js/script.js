212347feaa2f718a13fde44c2de5b00284011504
AdulIT <spilberg.max@bk.ru>
2021-05-25 13:18:03 +0600

    smooth scroll and pageup

--- src/js/script.js
+++ src/js/script.js
@@ -83,4 +83,20 @@ document.querySelector('.next').addEventListener('click', function () {
 
 	  $('input[name=phone]').mask("+7 (999) 999-99-99");
 	});
+
+	// Smooth scroll and pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function(){
+		var _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
 	})(jQuery);
